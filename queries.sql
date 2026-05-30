9d3ab3fbb95d1463510e77714eadbdd81f709ff4
فیضان احمد <forfaizanahmed@gmail.com>
2026-05-30 09:49:30 +0530

    Queries added just for testing

---
+++ queries.sql
@@ -0,0 +1,12 @@
+SELECT * FROM public."AspNetUsers"
+ORDER BY "Id" ASC;
+
+SELECT * FROM public."AspNetRoleClaims"; -- permissions
+SELECT * FROM public."AspNetRoles"; -- roles
+SELECT * FROM public."AspNetUserClaims"; -- konse user ko kya permissions hai
+SELECT * FROM public."AspNetUserRoles"; -- konse users ka kya role hai 
+
+-- NOT IN USE
+SELECT * FROM public."AspNetUserLogins";
+SELECT * FROM public."AspNetUserTokens";
+
